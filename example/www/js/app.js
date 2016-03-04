2a712c143f9cd5feb9c5e43739cf7dc97710537a
JorgeCoke <cokelas@gmail.com>
2016-03-04 12:06:48 +0100

    Implementación Leaflet + GUI
    - Integración Leaflet
    - Añadida funcionalidad a los botones del menu principal
    - Nueva GUI propuesta para el menu principal
    - Añadidos Menu de Capas y Acciones

--- example/www/js/app.js
+++ example/www/js/app.js
@@ -5,60 +5,133 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
+/**
+ * Controlador para los botones de la pantalla inicial:
+ * Ada Byron, Torres Quevedo, Betancourt.
+ *
+ * El controlador ajusta las coordenadas (x, y) para cada edificio y oculta los tres botones
+ *
+ * Se requiere $scope y $log
+ */
+.controller('ButtonsCtrl', ['$scope', '$log', function ($scope, $log) {
+  /* atributos para la latitud y longitud */
+  $scope.x = 0.0;
+  $scope.y = 0.0;
+
+  $scope.mostrarBotones = true;  // mostrar = false cuando se seleccione una opcion
+  $scope.$log = $log; // variable para acceder al log
+
+  /* funciones para ir al ada, al torres quevedo o al betancourt */
+
   /**
-   * Controlador para los botones de la pantalla inicial:
-   * Ada Byron, Torres Quevedo, Betancourt.
-   *
-   * El controlador ajusta las coordenadas (x, y) para cada edificio y oculta los tres botones
-   *
-   * Se requiere $scope y $log
+   * Coordenadas para ir al Ada Byron
    */
-  .controller('ButtonsCtrl', ['$scope', '$log', function ($scope, $log) {
-    /* atributos para la latitud y longitud */
-    $scope.x = 0.0;
-    $scope.y = 0.0;
+  $scope.ada = function () {
+    $scope.x = 41.683582;
+    $scope.y = -0.888826;
+    $scope.common();
+    $scope.cargarMapa($scope.x,$scope.y);
+  };
 
-    $scope.mostrarBotones = true;  // mostrar = false cuando se seleccione una opcion
-    $scope.$log = $log; // variable para acceder al log
+  /**
+   * Coordenadas para ir al Torres Quevedo
+   */
+  $scope.tq = function () {
+    $scope.x = 41.683561;
+    $scope.y = -0.887341;
+    $scope.common();
+    $scope.cargarMapa($scope.x,$scope.y);
+  };
 
-    /* funciones para ir al ada, al torres quevedo o al betancourt */
+  /**
+   * Coordenadas para ir al Betancourt
+   */
+  $scope.betan = function () {
+    $scope.x = 41.683622;
+    $scope.y = -0.883977;
+    $scope.common();
+    $scope.cargarMapa($scope.x,$scope.y);
+  };
 
-    /**
-     * Coordenadas para ir al Ada Byron
-     */
-    $scope.ada = function () {
-      $scope.x = 1.0;
-      $scope.y = 1.0;
-      $scope.common();
-    };
+  /**
+   * Agrupacion de operaciones comunes a ada(), tq() y betan()
+   */
+  $scope.common = function () {
+    $scope.mostrarBotones = false;
+    $log.log($scope.x + " " + $scope.y);
+  };
 
-    /**
-     * Coordenadas para ir al Torres Quevedo
-     */
-    $scope.tq = function () {
-      $scope.x = 1.0;
-      $scope.y = 1.0;
-      $scope.common();
-    };
+  $scope.cargarMapa = function (x,y){
+    /* Atributo para guardar el mapa */
+    var map = L.map('map');
+    //Creamos la capa base de OpenStreetMap
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 17, maxZoom: 22, attribution: osmAttrib});
+    //Inicializamos el mapa segun las coordenadas pulsadas
+    map.setView(new L.LatLng(x, y),18);
+    map.addLayer(osm);
+  };
+}])
 
-    /**
-     * Coordenadas para ir al Betancourt
-     */
-    $scope.betan = function () {
-      $scope.x = 1.0;
-      $scope.y = 1.0;
-      $scope.common();
+.controller('MenuAccionesCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('templates/menuAcciones.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
 
-    };
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
 
-    /**
-     * Agrupacion de operaciones comunes a ada(), tq() y betan()
-     */
-    $scope.common = function () {
-      $scope.mostrarBotones = false;
-      $log.log($scope.x + " " + $scope.y);
-    }
-  }])
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+})
+
+.controller('MenuCapasCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('templates/menuCapas.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+})
+
 
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
