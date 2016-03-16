da8889bf25e87d3b198f80c1b0f79cd5c30da2b3
Patricia Lazaro <plazaro1994@gmail.com>
2016-03-16 13:29:49 +0100

    bufffffff la pantalla del espacio esta hecha (pero inaccesible de momento)
    Falta, por otro lado, enlazar con los datos y demas asuntos

--- example/www/js/app.js
+++ example/www/js/app.js
@@ -114,6 +114,59 @@ angular.module('starter', ['ionic'])
   });
 })
 
+.controller('MenuAccionesEspacioCtrl', function($scope, $ionicModal) {
+  $scope.space = {  // TODO set it right
+    temperature : 20,
+    lights : false,
+    doors : true,
+    windows: false,
+    presence :true
+  };
+
+  $ionicModal.fromTemplateUrl('templates/menuAccionesEspacio.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+//TODO
+  $scope.moreTemperature = function(){
+    var t = $scope.space.temperature;
+    if(t + 1 <= 40){
+      $scope.space.temperature = t + 1;
+    }
+  };
+//TODO
+  $scope.lessTemperature = function(){
+    var t = $scope.space.temperature;
+    if(t - 1 >= 18){
+      $scope.space.temperature = t - 1;
+    }
+  };
+
+  /* Open and close the modal */
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+})
+
 .controller('MenuCapasCtrl', function($scope, $ionicModal) {
   $ionicModal.fromTemplateUrl('templates/menuCapas.html', {
     scope: $scope,
