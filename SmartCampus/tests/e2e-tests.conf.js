27d7d8225e3054a1f75a2b7301e217c51d3a1c0c
JorgeCoke <cokelas@gmail.com>
2016-03-30 12:42:00 +0200

    Code Coverage + Tutorial en la Wiki

--- SmartCampus/tests/e2e-tests.conf.js
+++ SmartCampus/tests/e2e-tests.conf.js
@@ -1,15 +1,43 @@
+var waitPlugin = require('./waitPlugin');
+var istanbul = require('istanbul');
+var collector = new istanbul.Collector();
+
 exports.config = {
-        capabilities: {
-            'browserName': 'chrome',
-            'chromeOptions': {
-                args: ['--disable-web-security']
-            }
-        },
-        baseUrl: 'http://localhost:8100',
-        specs: [
-            'e2e-tests/**/*.tests.js'
-        ],
-        jasmineNodeOpts: {
-            isVerbose: true,
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {
+      args: ['--disable-web-security']
+    }
+  },
+  baseUrl: 'http://localhost:8100',
+  specs: [
+    'e2e-tests/**/*.tests.js'
+  ],
+  jasmineNodeOpts: {
+    isVerbose: true,
+  },
+  framework: 'jasmine2',
+
+  //see https://github.com/angular/protractor/issues/1938#issuecomment-119690252
+  plugins: [{path: './waitPlugin.js'}],
+
+  onPrepare: function() {
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.addReporter(function() {
+      this.specDone = function(spec) {
+        if (spec.status !== 'failed') {
+          browser.driver.executeScript('return __coverage__;').then(function(coverageResults) {
+            collector.add(coverageResults);
+          });
         }
+      };
+    });
+  },
+  onComplete: function() {
+    istanbul.Report.create('cobertura', {dir: 'results/e2e/cobertura'})
+      .writeReport(collector, true);
+    istanbul.Report.create('lcov', {dir: 'results/e2e/lcov'})
+      .writeReport(collector, true);
+    waitPlugin.resolve();
+  }
 };
